a4548b7e2934292f8cdf1353e45b3ca5045048f2
bakoosh <ismiless@mail.ru>
2024-06-21 14:16:54 +0500

    fixed favourites

--- src/veiws/Favourites/Favourites.jsx
+++ src/veiws/Favourites/Favourites.jsx
@@ -1,23 +1,27 @@
-import React, {useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import axios from "axios";
 import Product from "../../components/Product";
+import { SearchContext } from "../../contexts/SearchContext";
 
 const Favourites = () => {
     const [products, setProducts] = React.useState([]);
+    const { searchValue } = useContext(SearchContext);
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/favourites').then((response) => {
             setProducts(response.data);
-        })
+        });
     }, []);
+
+    const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
 
     return (
         <div className={"w-full grid grid-cols-3 border-gray-400 h-screen"}>
-            {
-                products.map(product => {
-                    <Product product={product} key={product.id} />
-                })
-            }
+            {filteredProducts.map(product => (
+                <Product product={product} key={product.id} />
+            ))}
         </div>
     );
 };
