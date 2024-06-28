87f87514d372c800f439a186bc5badb87fd303ae
bakoosh <ismiless@mail.ru>
2024-06-28 14:59:09 +0500

    q

--- src/veiws/ProductInfo/ProductInfo.jsx
+++ src/veiws/ProductInfo/ProductInfo.jsx
@@ -46,6 +46,20 @@ const ProductInfo = () => {
                     <span className={"ml-7 text-gray-500 text-2xl"}>{product.size ? product.size : 'Размер не указан'}</span>
                 </div>
 
+                <div className={"flex items-center mt-5"}>
+                    <button className={"bg-gray-500 text-amber-50 font-bold text-md py-6 px-5 rounded-xl"}>Добавить в корзину
+                    </button>
+                    <div className={"bg-gray-500 ml-4 py-5 px-5 rounded-xl"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32.6266mm"
+                             height="26.2717mm"
+                             viewBox="0 0 150.93 121.54"
+                             className="fill-amber-50 w-8 h-8 hover:cursor-pointer  transition-transform duration-300 transform hover:scale-125 ">
+                            <path
+                                d="M75.47 121.54c-3.94,0 -7.89,-1.52 -10.9,-4.52l-53.71 -53.71c-14.49,-14.48 -14.49,-37.96 0,-52.44 7.22,-7.27 16.75,-10.88 26.23,-10.88 9.48,0 19.01,3.6 26.23,10.88l12.14 12.11 11.48 -11.48c7.44,-7.45 17.37,-11.26 27.21,-11.26 9.35,0 18.65,3.43 25.79,10.45 7.36,7.27 10.99,16.81 10.99,26.41 0,9.46 -3.59,18.96 -10.86,26.2l-53.71 53.71c-3.01,3 -6.96,4.52 -10.9,4.52z"/>
+                        </svg>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
