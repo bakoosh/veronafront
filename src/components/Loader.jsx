e1326d47bbcc03b8c90288c119b66f0e088c4f77
bakoosh <ismiless@mail.ru>
2024-06-28 15:39:43 +0500

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div className={"w-full flex items-center justify-center"}>
-            <div
-                className="flex items-center justify-centerinline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-                role="status">
-  <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] flex items-center justify-center"
-  >Loading...</span
-  >
-            </div>
+        <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+            <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-500 h-12 w-12"></div>
         </div>
     );
 };
--- src/veiws/ProductInfo/ProductInfo.jsx
+++ src/veiws/ProductInfo/ProductInfo.jsx
@@ -2,15 +2,25 @@ import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import {toast} from "react-toastify";
+import Product from "../Products/components/Product";
 
 const ProductInfo = () => {
     const {id} = useParams();
     const [product, setProduct] = React.useState('');
+    const [products, setProducts] = React.useState([]);
     useEffect(() => {
         axios.get(`http://127.0.0.1:8000/api/products/${id}`).then(response => {
             setProduct(response.data)
         })
     }, []);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/products/random').then(response => {
+            setProducts(response.data);
+        })
+    }, []);
+
+    console.log(products)
 
 
     const handleClick = async (product_id) => {
@@ -43,56 +53,76 @@ const ProductInfo = () => {
 
 
     return (
-        <div className={"flex items-center justify-between m-10"}>
-            <div className={"w-1/2"}>
-                <div className={"w-2/3 flex items-center justify-center mx-10 my-10"}>
-                    <img src="/catalogImages/браслеты.png" alt=""/>
+        <div className={"w-full min-h-screen"}>
+            <div className={"flex items-center justify-between m-10"}>
+                <div className={"w-1/2"}>
+                    <div className={"w-2/3 flex items-center justify-center mx-10 my-10"}>
+                        <img src="/catalogImages/браслеты.png" alt=""/>
+                    </div>
+                </div>
+                <div className={"w-[40%]"}>
+                    <h1 className={"text-4xl font-bold text-gray-500 flex items-start"}>{product.name}</h1>
+                    <span className={"font-bold text-gray-500 text-3xl flex items-start"}>{product.sample} проба</span>
+
+                    <div className={"mt-5"}>
+                        <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Артикул</h1>
+                        <div className={"w-full bg-blue-50 text-3xl text-gray-500 flex items-start py-1 px-2 mt-1"}>
+                            {product.vendor}
+                        </div>
+
+
+                        <h1 className={"text-2xl font-bold text-gray-500 flex items-start mt-5"}>Вес</h1>
+                        <div className={"w-full bg-blue-50 text-3xl text-gray-500 flex items-start py-1 px-2 mt-1"}>
+                            {product.average_weight} гр
+                        </div>
+                    </div>
+
+                    <div className={"flex items-center mt-5"}>
+                        <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Вставки</h1>
+                        <span className={"ml-7 text-gray-500 text-2xl"}>( {product.insert} )</span>
+                    </div>
+
+
+                    <div className={"flex items-center mt-5"}>
+                        <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Размер</h1>
+                        <span
+                            className={"ml-7 text-gray-500 text-2xl"}>{product.size ? product.size : 'Размер не указан'}</span>
+                    </div>
+
+                    <div className={"flex items-center mt-5"}>
+                        <button onClick={() => handleBasketClick(product.id)}
+                                className={"hover:cursor-pointer bg-gray-500 text-amber-50 font-bold text-md py-6 px-5 rounded-xl hover:text-gray-400 hover:transition  "}>Добавить
+                            в корзину
+                        </button>
+                        <div onClick={() => handleClick(product.id)}
+                             className={"bg-gray-500 ml-4 py-5 px-5 rounded-xl"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32.6266mm"
+                                 height="26.2717mm"
+                                 viewBox="0 0 150.93 121.54"
+                                 className="fill-amber-50 w-8 h-8 hover:cursor-pointer  transition-transform duration-300 transform hover:scale-125 ">
+                                <path
+                                    d="M75.47 121.54c-3.94,0 -7.89,-1.52 -10.9,-4.52l-53.71 -53.71c-14.49,-14.48 -14.49,-37.96 0,-52.44 7.22,-7.27 16.75,-10.88 26.23,-10.88 9.48,0 19.01,3.6 26.23,10.88l12.14 12.11 11.48 -11.48c7.44,-7.45 17.37,-11.26 27.21,-11.26 9.35,0 18.65,3.43 25.79,10.45 7.36,7.27 10.99,16.81 10.99,26.41 0,9.46 -3.59,18.96 -10.86,26.2l-53.71 53.71c-3.01,3 -6.96,4.52 -10.9,4.52z"/>
+                            </svg>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-            <div className={"w-[40%]"}>
-                <h1 className={"text-4xl font-bold text-gray-500 flex items-start"}>{product.name}</h1>
-                <span className={"font-bold text-gray-500 text-3xl flex items-start"}>{product.sample} проба</span>
-
-                <div className={"mt-5"}>
-                    <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Артикул</h1>
-                    <div className={"w-full bg-blue-50 text-3xl text-gray-500 flex items-start py-1 px-2 mt-1"}>
-                        {product.vendor}
-                    </div>
 
 
-                    <h1 className={"text-2xl font-bold text-gray-500 flex items-start mt-5"}>Вес</h1>
-                    <div className={"w-full bg-blue-50 text-3xl text-gray-500 flex items-start py-1 px-2 mt-1"}>
-                        {product.average_weight} гр
-                    </div>
-                </div>
+            <div className={"w-full h-1/3"}>
+                <h1 className={"text-gray-400 text-4xl mt-5"}>Может понравиться</h1>
 
-                <div className={"flex items-center mt-5"}>
-                    <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Вставки</h1>
-                    <span className={"ml-7 text-gray-500 text-2xl"}>( {product.insert} )</span>
-                </div>
-
-
-                <div className={"flex items-center mt-5"}>
-                    <h1 className={"text-2xl font-bold text-gray-500 flex items-start"}>Размер</h1>
-                    <span className={"ml-7 text-gray-500 text-2xl"}>{product.size ? product.size : 'Размер не указан'}</span>
-                </div>
-
-                <div className={"flex items-center mt-5"}>
-                    <button onClick={() => handleBasketClick(product.id)} className={"hover:cursor-pointer bg-gray-500 text-amber-50 font-bold text-md py-6 px-5 rounded-xl hover:text-gray-400 hover:transition  "}>Добавить в корзину
-                    </button>
-                    <div onClick={() => handleClick(product.id)} className={"bg-gray-500 ml-4 py-5 px-5 rounded-xl"}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32.6266mm"
-                             height="26.2717mm"
-                             viewBox="0 0 150.93 121.54"
-                             className="fill-amber-50 w-8 h-8 hover:cursor-pointer  transition-transform duration-300 transform hover:scale-125 ">
-                            <path
-                                d="M75.47 121.54c-3.94,0 -7.89,-1.52 -10.9,-4.52l-53.71 -53.71c-14.49,-14.48 -14.49,-37.96 0,-52.44 7.22,-7.27 16.75,-10.88 26.23,-10.88 9.48,0 19.01,3.6 26.23,10.88l12.14 12.11 11.48 -11.48c7.44,-7.45 17.37,-11.26 27.21,-11.26 9.35,0 18.65,3.43 25.79,10.45 7.36,7.27 10.99,16.81 10.99,26.41 0,9.46 -3.59,18.96 -10.86,26.2l-53.71 53.71c-3.01,3 -6.96,4.52 -10.9,4.52z"/>
-                        </svg>
-                    </div>
-
+                <div className={"grid grid-cols-4 gap-4"}>
+                    {/*{*/}
+                    {/*    product.map(product => {*/}
+                    {/*        <Product product={product} key={product.id} />*/}
+                    {/*    })*/}
+                    {/*}*/}
                 </div>
             </div>
         </div>
+
     );
 };
 
